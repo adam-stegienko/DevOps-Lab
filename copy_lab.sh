9abb8784dafd97801b57ca1776b9db15266e141a
Adam <adam.stegienko1@gmail.com>
2022-09-21 14:43:21 +0200

    Main dir reorganization

--- copy_lab.sh
+++ copy_lab.sh
@@ -3,8 +3,5 @@
 SECRET_KEY="adam-lab.pem"
 PUBLIC_DNS="ubuntu@ec2-3-67-195-219.eu-central-1.compute.amazonaws.com"
 
-scp -i $SECRET_KEY ./lab_files/docker-compose.yaml $PUBLIC_DNS:~/lab_files/
-scp -i $SECRET_KEY ./lab_files/Dockerfile $PUBLIC_DNS:~/lab_files/
-scp -i $SECRET_KEY ./lab_files/init.sh $PUBLIC_DNS:~/lab_files/
-scp -i $SECRET_KEY ./lab_files/dev_server_run.sh $PUBLIC_DNS:~/lab_files/
+scp -i $SECRET_KEY ./EC2 $PUBLIC_DNS:~/
 ssh -i $SECRET_KEY $PUBLIC_DNS
